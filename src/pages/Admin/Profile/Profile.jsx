34c3bc38e6c894200755c6284b7a0a3e842d1583
Zovarg <zovavrg@mail.ru>
2023-02-19 03:20:28 +0300

    upd-profile

--- src/pages/Admin/Profile/Profile.jsx
+++ src/pages/Admin/Profile/Profile.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import cl from './Profile.module.css'
 import AvatarProfile from "../../../images/AvatarProfile.svg";
+import photoPhoneProfile from "../../../images/photoPhoneProfile.svg";
 
 const Profile = () => {
     const [titles, setTitles]=useState([
@@ -102,6 +103,244 @@ const Profile = () => {
                         <div className={cl.profileStatsValue__naming}>Рейтинг (общий)</div>
                         <div className={cl.profileStatsValue__counter}>4,8 ⭐️</div>
                     </div>
+                </div>
+            </div>
+            <div className={cl.profileData}>
+                <div className={cl.profileData__header}>
+                    <div className={cl.profileData__header_title}>Данные профиля</div>
+                    <div className={cl.profileData__header_edit}>Редактировать профиль</div>
+                </div>
+                <div className={cl.profileBody}>
+                    <div className={cl.profileBody__fields}>
+                        <div className={cl.profileBody__field}>
+                            <div className={cl.profileBody__field_name}>ФИО</div>
+                            <div className={cl.profileBody__field_value}>Ткаченко И. Г.</div>
+                        </div>
+                        <div className={cl.profileBody__field}>
+                            <div className={cl.profileBody__field_name}>Организация</div>
+                            <div className={cl.profileBody__field_value}>ООО “Мега-кластер”</div>
+                        </div>
+                        <div className={cl.profileBody__field}>
+                            <div className={cl.profileBody__field_name}>Контактный телефон</div>
+                            <div className={cl.profileBody__field_value}>+7 (989) 999-89-89</div>
+                        </div>
+                        <div className={cl.profileBody__field}>
+                            <div className={cl.profileBody__field_name}>Email</div>
+                            <div className={cl.profileBody__field_value}>admin@re-control.com</div>
+                        </div>
+                        <div className={cl.profileBody__field}>
+                            <div className={cl.profileBody__field_name}>Город</div>
+                            <div className={cl.profileBody__field_value}>Краснодар</div>
+                        </div>
+                        <div className={cl.profileBody__field}>
+                            <div className={cl.profileBody__field_name}>Способ связи</div>
+                            <div className={cl.profileBody__field_value}>Телефон, email</div>
+                        </div>
+                        <div className={cl.profileBody__field}>
+                            <div className={cl.profileBody__field_name}>Разрешить изменения</div>
+                            <div className={cl.profileBody__field_value}>Да</div>
+                        </div>
+                    </div>
+                    <div className={cl.profileBody__success}>
+                        <div className={cl.profileBody__success_icon}>
+                            <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M11.1909 15.777C11.0048 15.5992 10.7574 15.5 10.5 15.5C10.4998 15.5 10.4773 15.5003 10.4773 15.5003C10.2122 15.5063 9.96027 15.6174 9.77704 15.8091C9.59923 15.9952 9.5 16.2426 9.5 16.5L9.50026 16.5227C9.50627 16.7878 9.61737 17.0397 9.80911 17.223L13.4716 20.723C13.8579 21.0921 14.4662 21.0924 14.8528 20.7236L22.19 13.7238C22.3819 13.5407 22.4935 13.2887 22.4997 13.0235C22.5001 13.0075 22.5001 12.9915 22.4997 12.9755C22.4936 12.727 22.3952 12.4896 22.2236 12.3097C22.0348 12.1119 21.7734 12 21.5 12L21.4718 12.0004C21.2245 12.0074 20.9887 12.1057 20.8097 12.2764L14.1631 18.6174L11.1909 15.777Z" fill="#1C1C1C"/>
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M16 3C16 3 18.6442 3 21.0605 4.02201C21.0605 4.02201 23.3936 5.00884 25.1924 6.80761C25.1924 6.80761 26.9912 8.60638 27.978 10.9395C27.978 10.9395 29 13.3558 29 16C29 16 29 18.6442 27.978 21.0605C27.978 21.0605 26.9912 23.3936 25.1924 25.1924C25.1924 25.1924 23.3936 26.9912 21.0605 27.978C21.0605 27.978 18.6442 29 16 29C16 29 13.3558 29 10.9395 27.978C10.9395 27.978 8.60638 26.9912 6.80761 25.1924C6.80761 25.1924 5.00884 23.3936 4.02202 21.0605C4.02202 21.0605 3 18.6442 3 16C3 16 3 13.3558 4.02202 10.9395C4.02202 10.9395 5.00885 8.60638 6.80761 6.80761C6.80761 6.80761 8.60638 5.00884 10.9395 4.02201C10.9395 4.02201 13.3558 3 16 3ZM16 5C16 5 13.7614 5 11.7186 5.86402C11.7186 5.86402 9.74476 6.69889 8.22183 8.22182C8.22183 8.22182 6.6989 9.74476 5.86402 11.7186C5.86402 11.7186 5 13.7614 5 16C5 16 5 18.2386 5.86402 20.2814C5.86402 20.2814 6.69889 22.2552 8.22183 23.7782C8.22183 23.7782 9.74476 25.3011 11.7186 26.136C11.7186 26.136 13.7614 27 16 27C16 27 18.2386 27 20.2814 26.136C20.2814 26.136 22.2552 25.3011 23.7782 23.7782C23.7782 23.7782 25.3011 22.2552 26.136 20.2814C26.136 20.2814 27 18.2386 27 16C27 16 27 13.7614 26.136 11.7186C26.136 11.7186 25.3011 9.74476 23.7782 8.22183C23.7782 8.22183 22.2552 6.69889 20.2814 5.86402C20.2814 5.86402 18.2386 5 16 5Z" fill="#1C1C1C"/>
+                            </svg>
+                        </div>
+                        <div className={cl.profileBody__success_description}>
+                            <div className={cl.topSuccessBody}>Всё хорошо!</div>
+                            <div className={cl.bottomSuccessBody}>Все данные заполнены и проверены. Никаких действий не требуется.</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={cl.profileTasksAndMobile}>
+                <div className={cl.profileTasks}>
+                    <div className={cl.profileTasks__header}>
+                        <div className={cl.profileTasks__header_name}>Ваши последние задачи</div>
+                        <div className={cl.profileTasks__header_value}>Все задачи</div>
+                    </div>
+                    <div>
+                        <table className="table">
+                            <thead>
+                            <tr>
+                                <th>Задача</th>
+                                <th>Поставлена</th>
+                                <th>Прошло времени</th>
+                                <th>Статус</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td className={cl.tdTasks}>
+                                        Создать аналитику
+                                    </td>
+                                    <td className={cl.tdTasks}>
+                                        18.02.2023
+                                    </td>
+                                    <td className={cl.tdTasks}>
+                                        3 ч 20 мин
+                                    </td>
+                                    <td>
+                                        <div className={cl.tableStatus1}>
+                                            <div>
+                                                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M11 8C11 9.65685 9.65685 11 8 11C6.34315 11 5 9.65685 5 8C5 6.34315 6.34315 5 8 5C9.65685 5 11 6.34315 11 8Z" fill="url(#paint0_linear_2313_6049)"/>
+                                                <defs>
+                                                    <linearGradient id="paint0_linear_2313_6049" x1="11" y1="7.76623" x2="4.99994" y2="7.78888" gradientUnits="userSpaceOnUse">
+                                                        <stop stop-color="#8244DC"/>
+                                                        <stop offset="0.0001" stop-color="#8244DC"/>
+                                                        <stop offset="0.515625" stop-color="#A756CD"/>
+                                                        <stop offset="1" stop-color="#BC6EC1"/>
+                                                    </linearGradient>
+                                                </defs>
+                                            </svg>
+                                            </div>
+                                            <div>В процессе</div>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td className={cl.tdTasks}>
+                                        Отобрать гипотезы
+                                    </td>
+                                    <td className={cl.tdTasks}>
+                                        18.02.2023
+                                    </td>
+                                    <td className={cl.tdTasks}>
+                                        12 ч 21 мин
+                                    </td>
+                                    <td>
+                                        <div className={cl.tableStatus2}>
+                                            <div>
+                                                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M11 8C11 9.65685 9.65685 11 8 11C6.34315 11 5 9.65685 5 8C5 6.34315 6.34315 5 8 5C9.65685 5 11 6.34315 11 8Z" fill="#A1E3CB"/>
+                                                </svg>
+                                            </div>
+                                            <div>Завершено</div>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td className={cl.tdTasks}>
+                                        Сгенерировать план
+                                    </td>
+                                    <td className={cl.tdTasks}>
+                                        17.02.2023
+                                    </td>
+                                    <td className={cl.tdTasks}>
+                                        26 ч 58 мин
+                                    </td>
+                                    <td>
+                                        <div className={cl.tableStatus3}>
+                                            <div>
+                                                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M11 8C11 9.65685 9.65685 11 8 11C6.34315 11 5 9.65685 5 8C5 6.34315 6.34315 5 8 5C9.65685 5 11 6.34315 11 8Z" fill="#FFE999"/>
+                                                </svg>
+                                            </div>
+                                            <div>В процессе</div>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td className={cl.tdTasks}>
+                                        Получить анализ
+                                    </td>
+                                    <td className={cl.tdTasks}>
+                                        16.02.2023
+                                    </td>
+                                    <td className={cl.tdTasks}>
+                                        17 ч 22 мин
+                                    </td>
+                                    <td>
+                                        <div className={cl.tableStatus4}>
+                                            <div>
+                                                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M11 8C11 9.65685 9.65685 11 8 11C6.34315 11 5 9.65685 5 8C5 6.34315 6.34315 5 8 5C9.65685 5 11 6.34315 11 8Z" fill="black" fill-opacity="0.4"/>
+                                                </svg>
+                                            </div>
+                                            <div>В процессе</div>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td className={cl.tdTasks}>
+                                        Заменить данные
+                                    </td>
+                                    <td className={cl.tdTasks}>
+                                        14.02.2023
+                                    </td>
+                                    <td className={cl.tdTasks}>
+                                        78 ч 05 мин
+                                    </td>
+                                    <td>
+                                        <div className={cl.tableStatus5}>
+                                            <div>
+                                                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M11 8C11 9.65685 9.65685 11 8 11C6.34315 11 5 9.65685 5 8C5 6.34315 6.34315 5 8 5C9.65685 5 11 6.34315 11 8Z" fill="#B1E3FF"/>
+                                                </svg>
+                                            </div>
+                                            <div>В процессе</div>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div className={cl.profileMobile}>
+                    <div className={cl.profileMobile__titles}>
+                        <div className={cl.profileMobile__title}>В приложении работать удобнее!</div>
+                        <div className={cl.profileMobile__title}>Не хотите попробовать?</div>
+                    </div>
+                    <div className={cl.profileMobile__photo}>
+                        <img src={photoPhoneProfile} alt=""/>
+                    </div>
+                    <div className={cl.profileMobile__btns}>
+                        <div className={cl.profileMobile__btn}>
+                            <div>
+                                <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <g clip-path="url(#clip0_2313_24148)">
+                                        <path fill-rule="evenodd" clip-rule="evenodd" d="M14.6334 4.5C14.759 5.35331 14.4117 6.18928 13.9535 6.78073C13.4633 7.41514 12.6184 7.90582 11.7998 7.88021C11.6503 7.06324 12.033 6.22149 12.4985 5.65565C13.0092 5.03115 13.8845 4.55204 14.6334 4.5ZM17.0927 16.5109C17.5147 15.8641 17.6724 15.5378 18 14.8075C15.6171 13.9014 15.2353 10.5137 17.5936 9.21352C16.8743 8.31147 15.8635 7.78857 14.9094 7.78857C14.2219 7.78857 13.7508 7.96798 13.3226 8.13108C12.9657 8.267 12.6386 8.39159 12.2408 8.39159C11.8108 8.39159 11.4301 8.25508 11.0314 8.11213C10.5933 7.95506 10.1336 7.79023 9.56312 7.79023C8.49239 7.79023 7.3527 8.44446 6.63012 9.56294C5.61441 11.1382 5.78766 14.0996 7.43481 16.6224C8.02354 17.5253 8.81016 18.5397 9.83901 18.5487C10.2659 18.553 10.55 18.4256 10.8574 18.2877C11.2093 18.1299 11.5917 17.9583 12.2539 17.9548C12.92 17.9508 13.2964 18.1244 13.6436 18.2846C13.9431 18.4227 14.2209 18.5508 14.6441 18.5463C15.6738 18.538 16.5039 17.4137 17.0927 16.5109Z" fill="#0B0B0A"/>
+                                    </g>
+                                    <defs>
+                                        <clipPath id="clip0_2313_24148">
+                                            <rect width="24" height="24" fill="white" transform="translate(0 0.5)"/>
+                                        </clipPath>
+                                    </defs>
+                                </svg>
+                            </div>
+                            <div>AppStore</div>
+                        </div>
+                        <div className={cl.profileMobile__btn}>
+                            <div>
+                                <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <g clip-path="url(#clip0_2313_24116)">
+                                        <path d="M0 12.5C0 5.87258 5.37258 0.5 12 0.5C18.6274 0.5 24 5.87258 24 12.5C24 19.1274 18.6274 24.5 12 24.5C5.37258 24.5 0 19.1274 0 12.5Z" fill="white"/>
+                                        <path d="M11.9999 8.82C13.1266 8.82 13.8866 9.30667 14.3199 9.71333L16.0132 8.06C14.9732 7.09333 13.6199 6.5 11.9999 6.5C9.65323 6.5 7.62656 7.84667 6.63989 9.80667L8.57989 11.3133C9.06656 9.86667 10.4132 8.82 11.9999 8.82V8.82Z" fill="#EA4335"/>
+                                        <path d="M17.76 12.6334C17.76 12.1401 17.72 11.7801 17.6333 11.4067H12V13.6334H15.3067C15.24 14.1867 14.88 15.0201 14.08 15.5801L15.9733 17.0467C17.1067 16.0001 17.76 14.4601 17.76 12.6334V12.6334Z" fill="#4285F4"/>
+                                        <path d="M8.58667 13.6866C8.46 13.3133 8.38667 12.9133 8.38667 12.5C8.38667 12.0866 8.46 11.6866 8.58 11.3133L6.64 9.80664C6.23333 10.62 6 11.5333 6 12.5C6 13.4666 6.23333 14.38 6.64 15.1933L8.58667 13.6866V13.6866Z" fill="#FBBC05"/>
+                                        <path d="M12.0001 18.5001C13.6201 18.5001 14.9801 17.9668 15.9734 17.0468L14.0801 15.5801C13.5734 15.9334 12.8934 16.1801 12.0001 16.1801C10.4134 16.1801 9.06673 15.1334 8.58673 13.6868L6.64673 15.1934C7.6334 17.1534 9.6534 18.5001 12.0001 18.5001V18.5001Z" fill="#34A853"/>
+                                    </g>
+                                    <defs>
+                                        <clipPath id="clip0_2313_24116">
+                                            <rect width="24" height="24" fill="white" transform="translate(0 0.5)"/>
+                                        </clipPath>
+                                    </defs>
+                                </svg>
+                            </div>
+                            <div>Google Play</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={cl.footerProfile}>
+                <div className={cl.footerProfile__copyright}>© 2023 re:Control</div>
+                <div className={cl.footerProfile__menu}>
+                    <div className={cl.footerProfile__menu_el}>О системе</div>
+                    <div className={cl.footerProfile__menu_el}>Поддержка</div>
+                    <div className={cl.footerProfile__menu_el}>База знаний</div>
                 </div>
             </div>
         </div>
